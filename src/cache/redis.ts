bcbeae7f62b8f49f7c5c95dd49d436a0b99234d8
khiemnee <khiemdeptrai2911@gmail.com>
2025-05-11 13:44:03 +0700

    deploy redis

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,9 +1,18 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+
+const client = createClient({
+    username: 'default',
+    password: '*******',
+    socket: {
+        host: 'redis-19997.crce194.ap-seast-1-1.ec2.redns.redis-cloud.com',
+        port: 19997
+    }
+});
 
 client.on('error', err => console.log('Redis Client Error', err));
 
 client.connect();
+
 
 export default client
