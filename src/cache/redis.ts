cc6ad842abca8cf671ac3de4b5310d54c21008bc
khiemnee <khiemdeptrai2911@gmail.com>
2025-05-11 13:45:55 +0700

    update redis file

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,9 +1,10 @@
 import { createClient } from 'redis';
 
 
+
 const client = createClient({
     username: 'default',
-    password: '*******',
+    password: 'oDVreALQpbx9MH1dx4IBzawPAPx94xt9',
     socket: {
         host: 'redis-19997.crce194.ap-seast-1-1.ec2.redns.redis-cloud.com',
         port: 19997
